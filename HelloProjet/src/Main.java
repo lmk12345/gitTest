703452e98ea75e0841e434aef9d24eb176105140
lmk12345 <2505412533@qq.com>
2019-08-07 09:41:47 +0800

    lisi有新代码提交

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
         System.out.println("新增代码需要提交");
+		System.out.println("lisi的新增代码需要提交");
     }
 }
